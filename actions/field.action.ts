5b705a3eaf6a62197c6949bb5f9f92f170bf6af3
miura-Miura <smiuymiu0official0@gmail.com>
2024-04-09 23:56:49 +0900

    add: field.actionのprisma対応(setFieldToUser,fetchFieldIdByFieldName)

--- actions/field.action.ts
+++ actions/field.action.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { fieldInterface } from "@/constants";
 import { prisma } from "@/lib/prisma/prisma-client";
 
@@ -13,7 +15,7 @@ export async function fetchAllFields(): Promise<fieldInterface[]> {
 }
 
 export async function fetchFieldsByUserId(
-  userId: string
+  userId: string,
 ): Promise<fieldInterface[]> {
   try {
     const affiliationsData = await prisma.$queryRaw<fieldInterface[]>`
@@ -22,11 +24,28 @@ export async function fetchFieldsByUserId(
         JOIN "_FieldsToUsers" ON "Fields".id = "_FieldsToUsers".field_id
         JOIN "Users" ON "_FieldsToUsers".user_id = "Users".id
         WHERE "Users".id = ${userId};`;
-
     return affiliationsData;
   } catch (error) {
     console.log(error);
     throw new Error("Failed to fetch fields.");
+  }
+}
+
+export async function fetchFieldIdByFieldName(
+  fieldData: fieldInterface,
+): Promise<number> {
+  try {
+    const fieldId = await prisma.$queryRaw<number>`
+      SELECT "id"
+      FROM "Fields"
+      WHERE name = ${fieldData.name};`;
+    if (fieldId == 0) {
+      return 0;
+    }
+    return fieldId;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch field id.");
   }
 }
 
@@ -38,5 +57,16 @@ export async function setField(fieldData: fieldInterface) {
   } catch (error) {
     console.log(error);
     throw new Error("Failed to set field.");
+  }
+}
+
+export async function setFieldToUser(field_id: number, user_id: string) {
+  try {
+    await prisma.$executeRaw`
+      INSERT INTO "_FieldsToUsers" (field_id, user_id)
+      VALUES (${field_id}, ${user_id});`;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to set field to user.");
   }
 }
