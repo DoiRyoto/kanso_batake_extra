e3406b3d66cdccc8928e536be7e73d030cc5524d
Miura <smiuymiu0official0@gmail.com>
2024-06-08 00:23:57 +0900

    update: setTagで並列に各タグをPOSTする
    for...of記法から、なんかいい方法に書き換えた。

--- actions/tag.action.ts
+++ actions/tag.action.ts
@@ -1,10 +1,9 @@
-import { tagInterface } from "@/constants";
 import { prisma } from "@/lib/prisma/prisma-client";
 import { Tag } from "@/type";
 
-export async function fetchAllTags(): Promise<tagInterface[]> {
+export async function fetchAllTags(): Promise<Tag[]> {
   try {
-    const tagsData = await prisma.$queryRaw<tagInterface[]>`
+    const tagsData = await prisma.$queryRaw<Tag[]>`
         SELECT * FROM "Tags"`;
 
     return tagsData;
@@ -13,11 +12,9 @@ export async function fetchAllTags(): Promise<tagInterface[]> {
   }
 }
 
-export async function fetchTagsByReviewId(
-  reviewId: number,
-): Promise<tagInterface[]> {
+export async function fetchTagsByReviewId(reviewId: number): Promise<Tag[]> {
   try {
-    const tagsData = await prisma.$queryRaw<tagInterface[]>`
+    const tagsData = await prisma.$queryRaw<Tag[]>`
         SELECT "Tags".*
         FROM "Tags"
         JOIN "_ReviewsToTags" ON "Tags".id = "_ReviewsToTags".tag_id
@@ -33,15 +30,16 @@ export async function fetchTagsByReviewId(
 
 export async function setTag(tags: Tag[]) {
   try {
-    for (const tagData of tags) {
-      await fetch("/api/tag/post", {
+    const req = tags.map((tag) =>
+      fetch("/api/tag/post", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ tagData }),
-      });
-    }
+        body: JSON.stringify({ tag }),
+      }),
+    );
+    await Promise.all(req);
   } catch (error) {
     console.log(error);
     throw new Error("Failed to set tag.");
@@ -51,19 +49,15 @@ export async function setTag(tags: Tag[]) {
 export async function setTagsToReview(reviewId: number, tags: Tag[]) {
   try {
     // tagを一個づつPOSTする
-    for (const tagData of tags) {
-      const body = {
-        tagData,
-        reviewId,
-      };
-      await fetch("/api/tagToreview/post", {
+    const req = tags.map((tag) =>
+      fetch("/api/tagToreview/post", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ body }),
-      });
-    }
+        body: JSON.stringify({ tag, reviewId }),
+      }),
+    );
   } catch (error) {
     console.log(error);
     throw new Error("Failed to set TagToReview table.");
