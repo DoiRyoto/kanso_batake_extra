218cc605328fed0d71378d056c11c9e0ec77bafb
miura-Miura <smiuymiu0official0@gmail.com>
2024-08-06 18:02:33 +0900

    update: レビュー取得時にユーザ情報を空配列にしない
    User型再定義に伴う影響のある範囲を修正
    Reviewを取得する際にworksなども取得して返すように更新

--- app/api/reviews/[id]/route.ts
+++ app/api/reviews/[id]/route.ts
@@ -114,11 +114,22 @@ async function fetchCommentsByReviewId(reviewId: number): Promise<Comments[]> {
   }
 }
 
-async function fetchUser(userId: string): Promise<Users[]> {
+async function fetchUser(userId: string): Promise<User[]> {
   try {
-    const userData = await prisma.$queryRaw<Users[]>`
-        SELECT * FROM "Users" WHERE id = ${userId};`;
-
+    const userData = await prisma.$queryRaw<User[]>`
+        SELECT
+          u.*,
+          json_agg(DISTINCT f.*) AS fields,
+          json_agg(DISTINCT w.*) AS works,
+          json_agg(DISTINCT a.*) AS affiliations
+        FROM "Users" u
+        LEFT JOIN "_FieldsToUsers" ftu ON u.id = ftu.user_id
+        LEFT JOIN "Fields" f ON ftu.field_id = f.id
+        LEFT JOIN "Works" w ON u.id = w.user_id
+        LEFT JOIN "_AffiliationsToUsers" atu ON u.id = atu.user_id
+        LEFT JOIN "Affiliations" a ON atu.affiliation_id = a.id
+        WHERE u.id = ${userId}
+        GROUP BY u.id, f.id, w.id, a.id;`;
     return userData;
   } catch (error) {
     console.log(error);
@@ -163,15 +174,7 @@ export async function GET(
       content: review[0].content,
       paper_title: review[0].paper_title,
       paper_data: review[0].paper_data as Paper,
-      user_info: {
-        id: user[0].id,
-        name: user[0].name,
-        role: user[0].role,
-        created_at: user[0].created_at,
-        works: [],
-        fields: [],
-        affiliations: [],
-      } as User,
+      user_info: user[0],
       comments: comments,
       tags: tags,
       created_at: review[0].created_at,
