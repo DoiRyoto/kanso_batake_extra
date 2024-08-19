b3f3d31d5ced554a8d51ea0cbcb8df9f3398b548
takumi-333 <takumi_soccer0514@icloud.com>
2024-08-19 23:49:07 +0900

    route.tsの変更

--- app/api/data_migrate/route.ts
+++ app/api/data_migrate/route.ts
@@ -1,57 +1,84 @@
 import { fetchCommentsByReviewIdByFB } from "@/actions/comment.action";
 import { fetchAllReviewsByFB, setReview } from "@/actions/review.action";
-import { fetchUserByFB } from "@/actions/user.action";
-import { Review, Tag, commentType, reviewType, userType } from "@/type";
+import {
+  fetchUserByFB,
+  fetchUsersByAffiliationId,
+  fetchUsersByFB,
+  setUser,
+} from "@/actions/user.action";
+import {
+  Review,
+  Tag,
+  Work,
+  commentType,
+  reviewType,
+  userType,
+  Field,
+  Affiliation,
+  User,
+} from "@/type";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   try {
-    const reviewDatas: reviewType[] = await fetchAllReviewsByFB();
-    reviewDatas.map(async (reviewData, idx) => {
-      const user: userType = await fetchUserByFB(reviewData.createdBy);
-      let tags: Tag[] = [];
-      if (reviewData.tags.length != 0) {
-        tags = reviewData.tags.map((tag) => {
+    const userDatas: userType[] = await fetchUsersByFB();
+    userDatas.map(async (userData, idx) => {
+      // init works
+      let works: Work[] = [];
+      if (userData.works && userData.works.length > 0) {
+        console.log("check works");
+        console.log(userData.works);
+        works = userData.works.map((work) => {
           return {
             id: -1,
-            name: tag,
+            url: work,
+            user_id: userData.id,
             created_at: Date(),
           };
         });
       }
 
-      const newReviewData: Review = {
-        id: Date.now() + idx,
-        content: reviewData.contents,
-        paper_title: reviewData.paperTitle,
-        user_info: {
-          id: user.id,
-          name: user.name,
-          role: user.role,
-          created_at: Date(),
-        },
-        paper_data: {
-          venue: reviewData.venue,
-          year: reviewData.year,
-          journal_name: reviewData.journal_name,
-          journal_pages: reviewData.journal_pages,
-          journal_vol: reviewData.journal_vol,
-          authors: reviewData.authors,
-          doi: reviewData.doi,
-          link: reviewData.link,
-        },
-        tags: tags,
-        thumbnail_url: reviewData.imageUrl,
+      // init fields
+      let fields: Field[] = [];
+      if (userData.field && userData.field.length != 0) {
+        fields = userData.field.map((field) => {
+          return {
+            id: -1,
+            name: field,
+            created_at: Date(),
+          };
+        });
+      }
+
+      // init affiliations
+      let affiliations: Affiliation[] = [];
+      if (userData.affiliation && userData.affiliation?.length != 0) {
+        affiliations = userData.affiliation.map((affiliation) => {
+          return {
+            id: -1,
+            name: affiliation,
+            created_at: Date(),
+          };
+        });
+      }
+
+      const newUserData: User = {
+        id: userData.id,
+        name: userData.name,
+        role: userData.role,
         created_at: Date(),
-        comments: [],
+        works: works,
+        affiliations: affiliations,
+        fields: fields,
       };
-      await setReview(newReviewData);
+
+      await setUser(newUserData);
     });
     return NextResponse.json({ status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: "Failed to migrate reviews." },
+      { error: "Failed to migrate users." },
       { status: 500 },
     );
   }
