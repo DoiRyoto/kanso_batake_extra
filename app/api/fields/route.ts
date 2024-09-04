e360c603167ac96d9c0a5e1aad79e59953657dd4
Miura <smiuymiu0official0@gmail.com>
2024-09-05 01:31:37 +0900

    add: /api/fields GET,POSTメソッドの実装

--- app/api/fields/route.ts
+++ app/api/fields/route.ts
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { Field } from "@/type";
 import { prisma } from "@/lib/prisma/prisma-client";
 
-async function fetchAllFields(): Promise<Field[]> {
+async function fetchField(): Promise<Field[]> {
   try {
     const fields: Field[] = await prisma.$queryRaw<Field[]>`
-      SELECT * FROM "Fields" ORDER BY id ASC;
+      SELECT * FROM "Fields";
     `;
     return fields;
   } catch (error) {
@@ -14,14 +14,42 @@ async function fetchAllFields(): Promise<Field[]> {
   }
 }
 
+async function setField(field: Field): Promise<Field[]> {
+  try {
+    const newTag = await prisma.$queryRaw<Field[]>`
+          INSERT INTO "Fields" (name)
+          VALUES (${field.name})
+          ON CONFLICT (name) DO nothing
+          RETURNING *;
+        `;
+    return newTag;
+  } catch (error) {
+    console.log(error);
+    throw new Error(`Failed to set field "${field.name}".`);
+  }
+}
+
 export async function GET(request: NextRequest): Promise<NextResponse> {
   try {
-    const fields = await fetchAllFields();
+    const fields = await fetchField();
     return NextResponse.json(fields, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
       { error: "Failed to fetch all fields." },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const params = await request.json();
+  try {
+    const newField = await setField(params);
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Failed to post Field` },
       { status: 500 },
     );
   }
